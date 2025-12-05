041b68bf174840a15a4cf19b00876ebe9312e8d4
Wellingtonpc17 <engwellingtoncavalcanti@gmail.com>
2025-12-05 00:42:18 -0300

    Update service worker cache version to v6

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,33 @@
-const CACHE_NAME = 'nura-v5';
+const CACHE_NAME = 'nura-v6';
 
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/firebase-auth.js',
-  '/scripts/script.js',
-  '/styles/style.css',
-  '/manifest.json',
+  './',
+  'index.html',
+  'firebase-auth.js',
+  'scripts/script.js',
+  'styles/style.css',
+  'manifest.json',
 
-  '/pages/cadastro.html',
-  '/pages/perfil.html',
-  '/pages/produtos.html',
-  '/pages/carrinho.html',
+  'pages/cadastro.html',
+  'pages/perfil.html',
+  'pages/produtos.html',
+  'pages/carrinho.html',
 
-  '/icons/favicon-192.png',
-  '/icons/favicon-512.png'
+  'icons/favicon-192.png',
+  'icons/favicon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
+
 
