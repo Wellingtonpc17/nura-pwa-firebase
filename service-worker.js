839e44140bb4a2012bcd4e28571263bc9ef8e5fe
Wellingtonpc17 <engwellingtoncavalcanti@gmail.com>
2025-12-05 00:32:52 -0300

    Update service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'nura-v3';
+const CACHE_NAME = 'nura-v5';
+
 const ASSETS = [
   '/',
   '/index.html',
-  '/styles/style.css',
+  '/firebase-auth.js',
   '/scripts/script.js',
+  '/styles/style.css',
   '/manifest.json',
 
   '/pages/cadastro.html',
@@ -11,20 +13,22 @@ const ASSETS = [
   '/pages/produtos.html',
   '/pages/carrinho.html',
 
-  './icons/favicon-192.png',
-  './icons/favicon-512.png'
+  '/icons/favicon-192.png',
+  '/icons/favicon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) =>
-      response || fetch(e.request)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
     )
   );
 });
